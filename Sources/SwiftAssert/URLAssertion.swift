8cd3ddc673b9f854976203a0fdf01eba0fe3d84c
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2020-02-26 15:25:04 +0200

    Update URLAssertion

--- Sources/SwiftAssert/URLAssertion.swift
+++ Sources/SwiftAssert/URLAssertion.swift
@@ -57,4 +57,76 @@ public extension Assertion where Subject == URL {
         }
         return self
     }
+    
+    @discardableResult
+    func hasUser(_ user: String, file: StaticString = #file, line: UInt = #line) -> Self {
+        if subject.user != user {
+            let failure = Failure(
+                text: "Expected \(subject) to have user '\(user)'",
+                location: SourceLocation(filePath: file, line: line)
+            )
+            FailureReporterHolder.sharedReporter.reportFailure(failure)
+        }
+        return self
+    }
+    
+    @discardableResult
+    func hasPassword(_ password: String, file: StaticString = #file, line: UInt = #line) -> Self {
+        if subject.password != password {
+            let failure = Failure(
+                text: "Expected \(subject) to have password '\(password)'",
+                location: SourceLocation(filePath: file, line: line)
+            )
+            FailureReporterHolder.sharedReporter.reportFailure(failure)
+        }
+        return self
+    }
+    
+    @discardableResult
+    func hasPort(_ port: Int, file: StaticString = #file, line: UInt = #line) -> Self {
+        if subject.port != port {
+            let failure = Failure(
+                text: "Expected \(subject) to have port '\(port)'",
+                location: SourceLocation(filePath: file, line: line)
+            )
+            FailureReporterHolder.sharedReporter.reportFailure(failure)
+        }
+        return self
+    }
+    
+    @discardableResult
+    func isFileURL(file: StaticString = #file, line: UInt = #line) -> Self {
+        if !subject.isFileURL {
+            let failure = Failure(
+                text: "Expected \(subject) to be file url",
+                location: SourceLocation(filePath: file, line: line)
+            )
+            FailureReporterHolder.sharedReporter.reportFailure(failure)
+        }
+        return self
+    }
+    
+    @discardableResult
+    func isNotFileURL(file: StaticString = #file, line: UInt = #line) -> Self {
+        if subject.isFileURL {
+            let failure = Failure(
+                text: "Expected \(subject) not to be file url",
+                location: SourceLocation(filePath: file, line: line)
+            )
+            FailureReporterHolder.sharedReporter.reportFailure(failure)
+        }
+        return self
+    }
+    
+    @discardableResult
+    func hasLastPathComponent(_ lastPathComponent: String, file: StaticString = #file, line: UInt = #line) -> Self {
+        assertThat(subject.lastPathComponent).isEqualTo(lastPathComponent, file: file, line: line)
+        return self
+    }
+    
+    @discardableResult
+    func hasPathExtension(_ pathExtension: String, file: StaticString = #file, line: UInt = #line) -> Self {
+        assertThat(subject.pathExtension).isEqualTo(pathExtension, file: file, line: line)
+        return self
+    }
 }
--- Tests/SwiftAssertTests/Test Cases/URLAssertion_Tests.swift
+++ Tests/SwiftAssertTests/Test Cases/URLAssertion_Tests.swift
@@ -74,4 +74,154 @@ class URLAssertion_Tests: AssertionTestCase {
         
         reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have path '/wrong'", at: location)
     }
+    
+    func test_WhenDoesNotConainUser_ShouldReportFailure() {
+        let url = URL(fileURLWithPath: #file)
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasUser("user")
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have user 'user'", at: location)
+    }
+    
+    func test_WhenUserDoesNotMatch_ShouldReportFailure() {
+        let url = URL(string: "test://wrong@test.com")!
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasUser("user")
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have user 'user'", at: location)
+    }
+    
+    func test_WhenUserDoesMatches_ShouldReportNoFailure() {
+        let url = URL(string: "test://user@test.com")!
+        
+        assertThat(url).hasUser("user")
+        
+        reporterMock.assertNoReportedFailures()
+    }
+    
+    func test_WhenPasswordIsMissing_ShouldReportFailuire() {
+        let url = URL(fileURLWithPath: #file)
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasPassword("password")
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have password 'password'", at: location)
+    }
+    
+    func test_WhenPasswordsDoNotMatch_ShouldReportFailuire() {
+        let url = URL(string: "test://user:wrongpassword@test.com")!
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasPassword("password")
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have password 'password'", at: location)
+    }
+    
+    func test_WhenPasswordsMatch_ShouldNoReportAnyFailure() {
+        let url = URL(string: "test://user:password@test.com")!
+        
+        assertThat(url).hasPassword("password")
+        
+        reporterMock.assertNoReportedFailures()
+    }
+    //
+    func test_WhenPortIsMissing_ShouldReportFailuire() {
+        let url = URL(fileURLWithPath: #file)
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasPort(1)
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have port '1'", at: location)
+    }
+    
+    func test_WhenPortsDoNotMatch_ShouldReportFailuire() {
+        let url = URL(string: "test://test.com")!
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasPort(1)
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to have port '1'", at: location)
+    }
+    
+    func test_WhenPortsMatch_ShouldNoReportAnyFailure() {
+        let url = URL(string: "test://test.com:1")!
+        
+        assertThat(url).hasPort(1)
+        
+        reporterMock.assertNoReportedFailures()
+    }
+}
+
+class URLAssertion_IsFileURL_TestCase: AssertionTestCase {
+    func test_WhenIsNotFileURL_ShouldReportFailure() {
+        let url = URL(string: "test://test.com")!
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).isFileURL()
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) to be file url", at: location)
+    }
+    
+    func test_WhenIsFileURL_ShouldNotReportAnyFailure() {
+        let url = URL(fileURLWithPath: #file)
+        
+        assertThat(url).isFileURL()
+        
+        reporterMock.assertNoReportedFailures()
+    }
+}
+
+class URLAssertion_IsNotFileURL_TestCase: AssertionTestCase {
+    func test_WhenIsNotFileURL_ShouldNotReportAnyFailure() {
+        let url = URL(string: "test://test.com")!
+        
+        assertThat(url).isNotFileURL()
+        
+        reporterMock.assertNoReportedFailures()
+    }
+    
+    func test_WhenIsFileURL_ShouldReportFailure() {
+        let url = URL(fileURLWithPath: #file)
+        
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).isNotFileURL()
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected \(url) not to be file url", at: location)
+    }
+}
+
+class URLAssertion_LastPathComponent_TestCase: AssertionTestCase {
+    let url = URL(string: "test://test.com/api/feed.json")!
+    
+    func test_WhenValuesDoNotMatch_ShouldReportFailure() {
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasLastPathComponent("wrong.json")
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected feed.json to equal to wrong.json", at: location)
+    }
+    
+    func test_WhenValuesMatch_ShouldReportNoFailure() {
+        assertThat(url).hasLastPathComponent("feed.json")
+        
+        reporterMock.assertNoReportedFailures()
+    }
+}
+
+class URLAssertion_PathExtension_TestCase: AssertionTestCase {
+    let url = URL(string: "test://test.com/api/feed.json")!
+    
+    func test_WhenValuesDoNotMatch_ShouldReportFailure() {
+        let location = createSourceLocationInThisFileAtTheNextLine()
+        assertThat(url).hasPathExtension("wrong")
+        
+        reporterMock.assertOneReportedFailure(withText: "Expected json to equal to wrong", at: location)
+    }
+    
+    func test_WhenValuesMatch_ShouldReportNoFailure() {
+        assertThat(url).hasPathExtension("json")
+        
+        reporterMock.assertNoReportedFailures()
+    }
 }
